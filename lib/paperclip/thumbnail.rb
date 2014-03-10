84929452bde580c13bdf9381a838990bde2f27cc
Michael Dotterer <michael@groupsite.com>
2014-03-10 11:25:16 -0400

    Use method instead of instance variable so that it uses the lazy loading properly

--- lib/paperclip/thumbnail.rb
+++ lib/paperclip/thumbnail.rb
@@ -74,7 +74,7 @@ module Paperclip
     # Returns the command ImageMagick's +convert+ needs to transform the image
     # into the thumbnail.
     def transformation_command
-      scale, crop = @current_geometry.transformation_to(@target_geometry, crop?)
+      scale, crop = current_geometry.transformation_to(@target_geometry, crop?)
       trans = []
       trans << "-resize" << scale unless scale.nil? || scale.empty?
       trans << "-crop" << crop << "+repage" if crop
